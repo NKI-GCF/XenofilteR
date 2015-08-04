078c81b98670da64eedb171f4a047cfd5558c3e8
Roel Kluin <r.kluin@nki.nl>
2015-08-04 17:24:31 +0200

    scripts used originally. xenofilt.sh and xenofilt_SE.pl are not yet tested so there could be issues.
    Signed-off-by: Roel Kluin <r.kluin@nki.nl>

---
+++ original/xenofilt.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+MM10_REF=$1
+HG38_REF=$2
+
+[ -z "$MM10_REF" ] && {
+cat << EOF
+
+Usage: ./xenofilt.sh xeno_reference.fasta host_reference.fasta
+
+    xeno is foreign tissue, e.g. human, hg38 reference
+    host is tissue, not of interest, e.g. mouse, mm10 reference
+
+    all reference files need to be indexed with 'bwa index' if you want to use bwa.
+    perl is required
+
+EOF
+exit 1
+}
+
+
+# install bwa and samtools, e.g.
+[ -z "$(which bwa)" ] && wget http://downloads.sourceforge.net/project/bio-bwa/bwa-0.7.12.tar.bz2
+[ -z "$(which samtools)" ] && wget http://downloads.sourceforge.net/project/samtools/samtools/0.1.19/samtools-0.1.19.tar.bz2
+
+ls -1 *.tar.bz2 | while read f; do tar xjf $f; cd ${f%.tar.bz2}; make; cd -; done
+[ -f bwa-0.7.12/bwa ] && alias bwa=bwa-0.7.12/bwa
+[ -f samtools-0.1.19/samtools ] && alias samtools=samtools-0.1.19/samtools
+
+# or if you want to use tophat/bowtie use that similarly.
+
+# align and make sure the human (hg38) is sorted. make sure resulting file is called
+# ${basename}-hg38.bam
+# see http://bio-bwa.sourceforge.net/bwa.shtml
+
+#your gzipped fastqs, if named differently change this and BN assignment below
+for FQ1 in *_R1_001.fastq.gz; do
+
+  BN=${FQ1%_R1_001.fastq.gz}
+
+  # use 1 gig for sorting
+  SAMMEM=1000000000
+  # use 4 cpus
+  NCPU=4
+
+  #your gzipped fastqs
+  FQ1=$(ls -1 ${BN}_R1_001.fastq.gz)
+
+  #read 2 if you have it, for paired-end sequencing
+  FQ2=$(ls -1 ${BN}_R2_001.fastq.gz 2>/dev/null)
+
+  #select xenofilt script accordingly
+  [ -z "$FQ2" ] && xenofilt_script=xenofilt_SE.pl || xenofilt_script=xenofilt_PE.pl
+
+  HG38_BAM=${BN}-hg38
+  HG38_SBAM=${BN}-hg38.bam
+
+  # indexing is not strictly necessary
+  bwa mem -M -t $NCPU -R "@RG\tID:$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c6)\tCN:NKI\tPL:ILLUMINA\tSM:${BN}\tLB:${BN}" $HG38_REF $FQ1 $FQ2 |
+  samtools view -Sbu - |
+  samtools sort -m $SAMMEM - $HG38_BAM
+  samtools index $HG38_BAM&
+
+  #indexing nor sort step is strictly necessary for mouse
+  MM10_BAM=${BN}-mm10
+  MM10_SBAM=${BN}-mm10.bam
+  bwa mem -M -t $NCPU -R "@RG\tID:$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c6)\tCN:NKI\tPL:ILLUMINA\tSM:${BN}\tLB:${BN}" $MM10_REF $FQ1 $FQ2 |
+  samtools view -Sbu - |
+  samtools sort -m $SAMMEM - $MM10_BAM
+  samtools index $MM10_SBAM&
+
+  HG38_FILT_BAM=${BN}-hg38-filt
+  HG38_FILT_SBAM=${BN}-hg38-filt.bam
+
+  perl ./$xenofilt_script $MM10_SBAM $HG38_SBAM |samtools view -S -b - | samtools sort -m 2G - $HG38_FILT_BAM
+  samtools index $HG38_FILT_SBAM&
+
+done
+
+
---
+++ original/xenofilt_SE.pl
@@ -0,0 +1,151 @@
+#!/usr/bin/env perl
+#===============================================================================
+#
+#         FILE: xenofilt.pl
+#
+#        USAGE: ./xenofilt.pl
+#
+#  DESCRIPTION: filter xenograft reads
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: (c) Roel 2014
+# ORGANIZATION:
+#      VERSION: 1.0
+#      CREATED: 01/10/2014 12:25:31 PM
+#     REVISION: ---
+#===============================================================================
+#
+# Loop over primary host (mouse) alignments. Store the number of mismatches plus the
+# number of clipped NTs multiplied by 1000 / read_quality. If the read is not mapped
+# instead assign the exceeding balance of readlength * 1000. The
+# readname + first/second [read in pair] are used as key.
+#
+# Subtract for the xenograft (human) alignments mismatches similarly for both reads.
+# When the balance is complete for both primary reads, we keep these reads if:
+# * both balances were positive, i.e. the (somewhat weighed) mismatches on the host
+# (mouse) were greater than on the xenograft (human). This is counted as
+# `matches_xeno_better'.
+# * the sum for both reads is still positive. This is `read_comb_matches_xeno_better'
+#
+# If primary alignments are printed, non-primary - stored for xeno in memory, some
+# pending - are printed as well.
+#
+#We loose the reads where this is not the case, or the result is `ambiguous'
+
+
+use strict;
+use warnings;
+use utf8;
+
+my $hostf = shift or die;
+my $samplef = shift or die;
+die unless -f $hostf;
+die unless -f $samplef;
+
+#die if -f $filtf;
+open (HOST, "samtools view '$hostf'|") or die "cannot samtools view $hostf:$!";
+open (SAMP, "samtools view -h '$samplef'|") or die "cannot samtools view $samplef:$!";
+
+my %h;
+sub print_stats {
+	my $stats = shift;
+	warn "count\tstat\n";
+	foreach my $t (sort {$a cmp $b} keys %$stats) {
+		warn join("\t", $stats->{$t}, $t)."\n";
+	}
+}
+# create tag for host parsing
+# store in hash entries which map perfectly to the host (e.g. mouse);
+
+# Iterate over de host mappings. Skip secondary alignments en unmapped reads tenzij een van beide reads wel mapt.
+# Voor beide reads sommeer het aantal mismatches en sla deze met de mapping quality op in een hash. De readname als key.
+HOSTLINE: while (<HOST>) {
+	chomp;
+	my @e = split /\t/;
+	next if $e[1] & 256; # ignore secondary alignments in host.
+	next if ($e[1] & 4) and $e[2] eq '*'; # unmapped; associated if $e[2] ne '*';
+	$e[0] =~ s/^HWI-[^:]++:[0-9]++:[^:]++://;
+
+	# sum mismatches (NM plus clips) and divide by quality
+	if (($e[4] == 0) || ($e[5] eq '*')) { # entire fail is 1000 * readlength
+		$h{$e[0]} = length($e[9]) * 1000;
+	} else {
+		my $i = $#e;
+		$i-- while (($i > 10) && ($e[$i] !~ /^NM:i:([0-9]+)$/));
+		if ($i != 10) {
+			$i = $1;
+			while ($e[5] =~ s/([0-9]++)S//o) { $i += $1; }
+			$h{$e[0]} = $i * 1000 / $e[4]; # matches * 1000 / read_quality
+		} else { # entire fail
+			$h{$e[0]} = length($e[9]) * 1000;
+		}
+	}
+}
+close HOST;
+warn "finished reading host...\n";
+
+my (%stats, %alt, $L, %single);
+while ($L = <SAMP>) {
+	last if $L !~ /^@/;
+	print $L; #no newline needed
+}
+while ($L) {
+	my $e = [split /\t/, $L];
+	chomp $e[$#e];
+	die unless $e->[0] =~ m/^HWI-[^:]++:[0-9]++:[^:]++:(.*)/;
+	if (exists $h{$1}) {
+		my $id = $1;
+		if ($e->[1] & 256) { # non primary, unused but printed along if others evaluated matchin xeno
+			if (not exists $single{$id}) {
+				push @{$alt{$id}}, $e;
+			} elsif ($single{$id} == 1) {
+				print "$L\n";
+			}
+			next;
+		}
+
+		# subtract HG19 mismatches (NM plus clips) divided by quality
+		if (($e->[4] == 0) || ($e->[5] eq '*')) {
+			$h{$id} -= length($e->[9]) * 1000;
+		} else {
+			my $i = $#$e;
+			$i-- while (($i > 10) && ($e->[$i] !~ /^NM:i:([0-9]+)$/));
+			if ($i != 10) {
+				$i = $1;
+				my $cig = $e->[5];
+				while ($cig =~ s/([0-9]++)S//o) { $i += $1; }
+				$h{$id} -= $i * 1000 / $e->[4];
+			} else {
+				$h{$id} -= length($e->[9]) * 1000;
+			}
+		}
+		die if exists $single{$id};
+		my $balance = $h{$id};
+		if ($balance > 0) { # both say it's more likely human
+			$stats{matches_xeno_better}++;
+			if (exists $alt{$id}) {
+				print join("\t", @{$_})."\n" foreach @{$alt{$id}};
+				delete $alt{$id};
+			}
+			print $L;
+			$single{$id} = 1;
+		} elsif ($balance < 0) {
+			$stats{matches_host_better}++;
+			$single{$id} = 0;
+		} else {
+			$single{$id} = 0;
+			$stats{ambiguous}++;
+		}
+	} else { # not mapped on host
+		print $L;
+		$stats{xeno_mapct}++;
+	}
+} continue {
+	$L = <SAMP>;
+}
+close SAMP;
+print_stats(\%stats);
+
